c2bf88f100569c8ace100bc922e28f1babdf45d2
Lucas Outeiro <lucasouteiro@gmail.com>
2022-09-05 11:53:51 -0300

    first commit

---
+++ assets/script/interface.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded',() =>{
+let squares = document.querySelectorAll(".square");
+
+squares.forEach((square)=> {
+    square.addEventListener('click', handleClick);
+
+})
+
+} )
+
+function handleClick(event){
+
+let square = event.target;
+let position = square.id;
+
+if (handleMove(position)){
+   
+
+    setTimeout(() =>{
+    alert("O jogador " + playerTime + " Venceu!!!");
+
+    }, 10);
+
+}
+updateSquare(position);
+
+}
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+function reset(){
+    window.location.reload();
+} 
+
+document.getElementById("reset").addEventListener("click", reset);
